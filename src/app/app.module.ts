2a1ccbbf69599429465f888d1010e37db05b086b
kmhoran <kevin.michael.horan@gmail.com>
2018-05-27 18:01:10 -0700

    add first component branches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ScheduleComponent,
+    ContactComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
